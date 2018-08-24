4ceedfc0748a1aea5c6617d45616eb462b9af678
ajain20 <anikajain99@outlook.com>
2018-08-24 14:46:26 +0100

    final transfer from gitlab

---
+++ CM1202Team24Prototype-master/CourseworkDQS/src/courseworddqs/CourseworkDQS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package courseworddqs;
+
+/**
+ *
+ * @author andrewbolton
+ */
+public class CourseworkDQS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home main = new Home();
+        main.setLocationRelativeTo(null);
+        main.setVisible(true);
+    }
+    
+}
